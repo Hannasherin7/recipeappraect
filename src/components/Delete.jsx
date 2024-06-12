59890bf20d4ee1423f0c01b98d49a5c019551a66
hannasherin <hannasherin7110@gmail.com>
2024-06-12 14:32:44 +0530

    message

---
+++ src/components/Delete.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import { Nav } from './Nav'
+
+export const Delete = () => {
+    const [data,setData]=useState(
+        {
+          "title":""
+        }
+      )
+      const inputHandler=(event)=>{
+        setData({...data,[event.target.name]:event.target.value})
+      }
+      const readVlue=()=>{
+        console.log(data)
+      }
+  return (
+    <div>
+        <h1><center>DELETE RECIPE</center></h1>
+        <Nav></Nav>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div className="row">
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label htmlFor="" className="form-label">title</label>
+                    <input type="text" className="form-control"name='title' value={data.title}onChange={inputHandler}/>
+                    </div>
+                    <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                      <br /><br />  <button onClick={readVlue} className="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
